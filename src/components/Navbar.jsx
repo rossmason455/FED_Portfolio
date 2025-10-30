7a8a6ea9d1942e30f99b3a62b684be3e2f3719b1
Ross Mason <N00230645@iadt.ie>
2025-10-30 13:40:30 +0000

    added country card component file and added logic for displaying countries in home.jsx from rest countries and added singleCountry.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router';
 
 export default function(){
     return (
-        <div>
+        <div >
             <Link to='/'>Home</Link> | 
             <Link to='about'>About</Link> |
             <Link to='contact'>Contact</Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import CountryCard from '../components/CountryCard';
+
+
+
 export default function Home(){
-    return(
-        <h1>Hello from Home page</h1>
+       const [countriesList, setCountriesList] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://restcountries.com/v3.1/all?fields=flags,flag,name,capital,cca3')
+             .then(response => {
+                console.log(response.data);
+                setCountriesList(response.data);
+             })
+             .catch(error => console.log(error));
+    }, []);
+
+    let countryCards = countriesList.map((country) => {
+        return (
+                <CountryCard 
+                    key={country.cca3}
+                    flagImg={country.flags.png}
+                    name={country.name.common}
+                    capital={country.capital}
+                />
+        );
+    });
+
+    return (
+        <div>
+            <h1>Home</h1>
+            {countryCards}
+        </div>
     );
 }
