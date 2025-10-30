8554aa69407f95bf5e22482124b1790787e16ff8
Ross Mason <N00230645@iadt.ie>
2025-10-30 19:31:55 +0000

    created SubRegionCountries page that retrieves all countries from a specified subregion and renders and displays their country cards

---
+++ src/pages/SubRegionCountries.jsx
@@ -0,0 +1,39 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import CountryCard from "../components/CountryCard";
+
+export default function SubregionCountries() {
+  const { subregionName } = useParams();
+  const [countryList, setCountryList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://restcountries.com/v3.1/subregion/${subregionName}?fields=flags,name,capital,cca3`
+      )
+      .then((response) => setCountryList(response.data))
+      .catch((error) => console.error(error));
+  }, [subregionName]);
+
+   const countryCards = countryList.map((country) => (
+    <CountryCard
+      key={country.cca3}
+      flagImg={country.flags.png}
+      name={country.name.common}
+      capital={country.capital}
+    />
+  ));
+
+   return (
+    <div>
+      <h1>{subregionName}</h1>
+      
+        {countryCards}
+      
+    </div>
+  );
+
+
+
+}
