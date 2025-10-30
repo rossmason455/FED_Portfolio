40ea5e1c200fca9d124cbcc455616db4d9c8cb0e
Ross Mason <N00230645@iadt.ie>
2025-10-30 19:01:57 +0000

    added logic to SubRegions that retrieves all subregions from each country object, uses filter(boolean) to filter out falsey values uses set to remove duplicates and converts contents back into an array. also added styling to SubRegionCard component

--- src/components/SubRegionCard.jsx
+++ src/components/SubRegionCard.jsx
@@ -1,1 +1,9 @@
-export default function SubregionCard() {}
+export default function SubregionCard({ subregionName }) {
+
+    return (
+    <div className="border rounded-lg p-4 shadow hover:shadow-lg transition-shadow">
+      <h2 className="text-xl font-semibold text-center">{subregionName}</h2>
+    </div>
+  );
+
+}
--- src/pages/SubRegions.jsx
+++ src/pages/SubRegions.jsx
@@ -1,7 +1,26 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import SubregionCard from "../components/SubregionCard";
+import SubRegionCard from "../components/SubRegionCard";
 
+export default function Subregions() {
+  const [subregionsList, setSubregionsList] = useState([]);
 
-export default function Subregions() {}
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all?fields=subregion")
+      .then((response) => {
+        const countries = response.data;
+        const uniqueSubregions = [
+          ...new Set(countries.map((country) => country.subregion).filter(Boolean)),
+        ];
+
+        const subregionObjects = uniqueSubregions.map((subregion) => ({
+          subregionName: subregion,
+        }));
+
+        setSubregionsList(subregionObjects);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+}
