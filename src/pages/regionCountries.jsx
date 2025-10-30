dc4a0a592fca4cdc99454aaf1e364b89dbfb2e90
Ross Mason <N00230645@iadt.ie>
2025-10-30 18:22:13 +0000

    In regionCountry.jsx added logic to grab all countries that belong to the specified region, stores the data in a state and maps the data to a list of country cards and displays them

--- src/pages/regionCountries.jsx
+++ src/pages/regionCountries.jsx
@@ -4,4 +4,37 @@ import axios from "axios";
 import CountryCard from "../components/CountryCard";
 
 
-export default function RegionCountries() {}
+export default function RegionCountries() {
+    const { regionName } = useParams();
+     const [countryList, setCountryList] = useState([]);
+
+    useEffect(() => { 
+    axios
+      .get(`https://restcountries.com/v3.1/region/${regionName}?fields=flags,name,capital,cca3`)
+      .then((response) => {
+        setCountryList(response.data);
+      })
+      .catch((error) => console.error(error));}, [regionName]);
+
+
+      const countryCards = countryList.map((country) => (
+    <CountryCard
+      key={country.cca3}
+      flagImg={country.flags.png}
+      name={country.name.common}
+      capital={country.capital}
+    />
+  ));
+
+   return (
+    <div>
+      <h1>{regionName}</h1>
+      {countryCards}
+    </div>
+  );
+}
+
+
+
+
+
