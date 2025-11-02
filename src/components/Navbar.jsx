4f4ea8cb9bf074a0c5d638ec73a07caa3d24006e
Ross Mason <N00230645@iadt.ie>
2025-11-02 12:27:34 +0000

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,14 @@ import { Link } from 'react-router';
 
 export default function(){
     return (
+        <>
+        <nav className="navbar bg- shadow">
         <div >
             <Link to='/'>Home</Link> | 
             <Link to='subregions'>Sub Regions</Link> |
             <Link to='regions'>Regions</Link>
         </div>
+        </nav>
+        </>
     );
 }
