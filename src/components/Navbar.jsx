fafe45c3593584661446ce0abb8a2d355213ccf1
Ross Mason <N00230645@iadt.ie>
2025-11-02 22:21:47 +0000

    removed styling test div

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,10 @@
-import { Link } from 'react-router';
+import { Link } from "react-router";
 
-export default function(){
-    return (
-        <>
-        <div className="bg-red-500 text-3xl font-bold p-8">
-            TESTING TAILWIND STYLES
-        </div>
-        <div >
-            <Link to='/'>Home</Link> | 
-            <Link to='subregions'>Sub Regions</Link> |
-            <Link to='regions'>Regions</Link>
-        </div>
-        
-        </>
-    );
+export default function () {
+  return (
+    <div>
+      <Link to="/">Home</Link> |<Link to="subregions">Sub Regions</Link> |
+      <Link to="regions">Regions</Link>
+    </div>
+  );
 }
